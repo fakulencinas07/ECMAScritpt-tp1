7b48b7e00ef470abd37671ef6753c7344866ad5c
fakulencinas07 <fakulencinas@gmail.com>
2023-10-23 16:52:53 -0300

    v2

--- backendtp1.js
+++ backendtp1.js
@@ -9,6 +9,11 @@ class ProductManager {
         
         const verificar = this.products.some(product => product.code === code);
 
+        if (verificar || !title || !description || !price || !thumbnail || !code || !stock) {
+            throw new Error("Todos los datos son obligatorios y no deben repetirse");
+        }
+
+
         const newProduct = {
             id: this.nextProductId,
             title,
@@ -37,7 +42,19 @@ class ProductManager {
     };
 }
 
-let productito = new ProductManager();
-productito.addProduct("coca", "asdasdasd", 1000, ".jpg", "1212", "100");
+const productito = new ProductManager();
+productito.addProduct("coca", "gaseosa", 1000, ".jpg", "1212", "100");
 console.log(productito.getProducts(1));
 console.log(productito.getProductById(1));
+
+console.log("Todos los productos:", productito.getProducts());
+
+
+const productById = productito.getProductById(1);
+console.log("Producto por ID:", productById);
+
+const nonExistentProduct = productito.getProductById(1);
+
+if (nonExistentProduct === null) {
+    console.error("Producto no encontrado");
+}
